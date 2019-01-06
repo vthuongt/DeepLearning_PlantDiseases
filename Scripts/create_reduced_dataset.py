5f21e4d9fd275d7e0e5f7efe344dac6c87abe0d6
vthuongt <viet.tran@uni-ulm.de>
2019-01-06 01:44:21 +0100

    ADD script to reduce trainig data for prototyping

---
+++ Scripts/create_reduced_dataset.py
@@ -0,0 +1,34 @@
+from os.path import join as osjoin
+import os
+import ipdb
+import pathlib
+from math import floor
+from shutil import copyfile
+
+# copyfile(src, dst)
+
+
+# src_path = r'C:\Users\vthuo\DeepLearning_PlantDiseases\Scripts\PlantVillage'
+src_path = r'PlantVillage'
+
+keep_factor = 0.10
+
+for root, dirs, files in os.walk(src_path):
+    # ipdb.set_trace()
+    if files:
+        dst_path = root.replace('PlantVillage', 'PlantVillage_reduced')
+        pathlib.Path(dst_path).mkdir(parents=True, exist_ok=True)
+        print('created: ' + dst_path)
+        files = files[:floor(keep_factor*len(files))]
+
+        for f in files:
+            copyfile(osjoin(root,f), osjoin(dst_path, f))
+
+
+
+
+
+
+
+
+
